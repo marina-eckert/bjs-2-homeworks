2d158bc59d7690ad9adf9fa1b16eb15918680ad9
marina-eckert <m.aap@inbox.ru>
2026-03-17 17:42:58 +0100

    Основные понятия - Задачи 1 и 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,39 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  const D = b * b - 4 * a * c;
+
+  if (D < 0) {
+    return arr;
+  }
+
+  const divisor = 2 * a;
+
+  if (D === 0) {
+    arr.push(-b / divisor);
+    return arr;
+  }
+
+  const sqrtD = Math.sqrt(D);
+  arr.push((-b + sqrtD) / divisor, (-b - sqrtD) / divisor);
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthly = percent / 1200;
+  const loan = amount - contribution;
+
+  if (loan <= 0) {
+    return 0;
+  }
+
+  const pow = Math.pow(1 + monthly, countMonths);
+  const payment = (loan * (monthly * pow)) / (pow - 1);
+
+  const total = payment * countMonths;
+
+  return Number(total.toFixed(2));
 }
